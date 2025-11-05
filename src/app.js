939d087037ba57e0d6df741b4c5b045dcea919f0
abhi-awale <abhishekawale.pune@gmail.com>
2025-11-05 17:20:06 +0530

    cors handled

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const swaggerUi = require("swagger-ui-express");
+const cors = require('cors');
+
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const testRoutes = require('./routes/test.routes');
-const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("../swagger");
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL, // allow both dev & prod
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // must include Authorization if using JWT
+  credentials: true, // allow cookies / auth headers
+}));
 
 app.use('/api/test', testRoutes);
 app.use('/api/auth', authRoutes);
